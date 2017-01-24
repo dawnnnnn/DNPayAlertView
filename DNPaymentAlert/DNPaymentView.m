28d91df2d65a8281f928050e8b9206a44f8a2c40
dawnnnnn <tan32211@gmail.com>
2017-01-24 22:58:50 +0800

    rename define and static constant

--- DNPaymentAlert/DNPaymentView.m
+++ DNPaymentAlert/DNPaymentView.m
@@ -8,24 +8,36 @@
 
 #import "DNPaymentView.h"
 
-#define TITLE_HEIGHT 46
-#define PAYMENT_WIDTH [UIScreen mainScreen].bounds.size.width-80
-#define PWD_COUNT 6
-#define DOT_WIDTH 10
-#define KEYBOARD_HEIGHT 216
-#define KEY_VIEW_DISTANCE 100
-#define ALERT_HEIGHT 200
+#define kSCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define kSCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define kPAYMENT_WIDTH kSCREEN_WIDTH-80
 
+static NSInteger kPasswordCount = 6;
 
-@interface DNPaymentView ()<UITextFieldDelegate>
-{
-    NSMutableArray *pwdIndicatorArr;
-}
-@property (nonatomic, strong) UIView *paymentAlert, *inputView;
+static CGFloat kTitleHeight     = 46.f;
+static CGFloat kDotWidth        = 10;
+static CGFloat kKeyboardHeight  = 216;
+static CGFloat kAlertHeight     = 200;
+static CGFloat kCommonMargin    = 100;
+
+@interface DNPaymentView () <UITextFieldDelegate>
+
+@property (nonatomic, strong) UIView *paymentAlert;
+@property (nonatomic, strong) UIView *inputView;
 @property (nonatomic, strong) UIButton *closeBtn;
-@property (nonatomic, strong) UILabel *titleLabel, *line, *detailLabel, *amountLabel;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *line;
+@property (nonatomic, strong) UILabel *detailLabel;
+@property (nonatomic, strong) UILabel *amountLabel;
 @property (nonatomic, strong) UITextField *pwdTextField;
+
 @property (nonatomic, strong) UIWindow *showWindow;
+
+@end
+
+@interface DNPaymentView ()
+
+@property (nonatomic, strong) NSMutableArray <UILabel *> *pwdIndicators;
 
 @end
 
@@ -44,66 +56,66 @@
 
 - (void)drawView {
     if (!_paymentAlert) {
-        _paymentAlert = [[UIView alloc]initWithFrame:CGRectMake(40, [UIScreen mainScreen].bounds.size.height-KEYBOARD_HEIGHT-KEY_VIEW_DISTANCE-ALERT_HEIGHT, [UIScreen mainScreen].bounds.size.width-80, ALERT_HEIGHT)];
+        _paymentAlert = [[UIView alloc]initWithFrame:CGRectMake(40, [UIScreen mainScreen].bounds.size.height-kKeyboardHeight-kCommonMargin-kAlertHeight, [UIScreen mainScreen].bounds.size.width-80, kAlertHeight)];
         _paymentAlert.layer.cornerRadius = 5.f;
         _paymentAlert.layer.masksToBounds = YES;
         _paymentAlert.backgroundColor = [UIColor colorWithWhite:1. alpha:.95];
         [self.view addSubview:_paymentAlert];
         
-        _titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, PAYMENT_WIDTH, TITLE_HEIGHT)];
+        _titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, kPAYMENT_WIDTH, kTitleHeight)];
         _titleLabel.textAlignment = NSTextAlignmentCenter;
         _titleLabel.textColor = [UIColor darkGrayColor];
         _titleLabel.font = [UIFont systemFontOfSize:17];
         [_paymentAlert addSubview:_titleLabel];
         
         _closeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_closeBtn setFrame:CGRectMake(0, 0, TITLE_HEIGHT, TITLE_HEIGHT)];
+        [_closeBtn setFrame:CGRectMake(0, 0, kTitleHeight, kTitleHeight)];
         [_closeBtn setTitle:@"╳" forState:UIControlStateNormal];
         [_closeBtn setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
         [_closeBtn addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
         _closeBtn.titleLabel.font = [UIFont systemFontOfSize:15];
         [_paymentAlert addSubview:_closeBtn];
         
-        _line = [[UILabel alloc]initWithFrame:CGRectMake(0, TITLE_HEIGHT, PAYMENT_WIDTH, .5f)];
+        _line = [[UILabel alloc]initWithFrame:CGRectMake(0, kTitleHeight, kPAYMENT_WIDTH, .5f)];
         _line.backgroundColor = [UIColor lightGrayColor];
         [_paymentAlert addSubview:_line];
         
-        _detailLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, TITLE_HEIGHT+15, PAYMENT_WIDTH-30, 20)];
+        _detailLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, kTitleHeight+15, kPAYMENT_WIDTH-30, 20)];
         _detailLabel.textAlignment = NSTextAlignmentCenter;
         _detailLabel.textColor = [UIColor darkGrayColor];
         _detailLabel.font = [UIFont systemFontOfSize:16];
         [_paymentAlert addSubview:_detailLabel];
         
-        _amountLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, TITLE_HEIGHT*2, PAYMENT_WIDTH-30, 25)];
+        _amountLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, kTitleHeight*2, kPAYMENT_WIDTH-30, 25)];
         _amountLabel.textAlignment = NSTextAlignmentCenter;
         _amountLabel.textColor = [UIColor darkGrayColor];
         _amountLabel.font = [UIFont systemFontOfSize:33];
         [_paymentAlert addSubview:_amountLabel];
         
-        _inputView = [[UIView alloc]initWithFrame:CGRectMake(15, _paymentAlert.frame.size.height-(PAYMENT_WIDTH-30)/6-15, PAYMENT_WIDTH-30, (PAYMENT_WIDTH-30)/6)];
+        _inputView = [[UIView alloc]initWithFrame:CGRectMake(15, _paymentAlert.frame.size.height-(kPAYMENT_WIDTH-30)/6-15, kPAYMENT_WIDTH-30, (kPAYMENT_WIDTH-30)/6)];
         _inputView.backgroundColor = [UIColor whiteColor];
         _inputView.layer.borderWidth = 1.f;
         _inputView.layer.borderColor = [UIColor colorWithRed:.9 green:.9 blue:.9 alpha:1.].CGColor;
         [_paymentAlert addSubview:_inputView];
         
-        pwdIndicatorArr = [[NSMutableArray alloc]init];
+        
         _pwdTextField = [[UITextField alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
         _pwdTextField.hidden = YES;
         _pwdTextField.delegate = self;
         _pwdTextField.keyboardType = UIKeyboardTypeNumberPad;
         [_inputView addSubview:_pwdTextField];
         
-        CGFloat width = _inputView.bounds.size.width/PWD_COUNT;
-        for (int i = 0; i < PWD_COUNT; i ++) {
-            UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake((width-DOT_WIDTH)/2.f + i*width, (_inputView.bounds.size.height-DOT_WIDTH)/2.f, DOT_WIDTH, DOT_WIDTH)];
+        CGFloat width = _inputView.bounds.size.width/kPasswordCount;
+        for (int i = 0; i < kPasswordCount; i ++) {
+            UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake((width-kDotWidth)/2.f + i*width, (_inputView.bounds.size.height-kDotWidth)/2.f, kDotWidth, kDotWidth)];
             dot.backgroundColor = [UIColor blackColor];
-            dot.layer.cornerRadius = DOT_WIDTH/2.;
+            dot.layer.cornerRadius = kDotWidth/2.;
             dot.clipsToBounds = YES;
             dot.hidden = YES;
             [_inputView addSubview:dot];
-            [pwdIndicatorArr addObject:dot];
+            [self.pwdIndicators addObject:dot];
             
-                if (i == PWD_COUNT-1) {
+                if (i == kPasswordCount-1) {
                 continue;
             }
             UILabel *line = [[UILabel alloc]initWithFrame:CGRectMake((i+1)*width, 0, .5f, _inputView.bounds.size.height)];
@@ -151,7 +163,7 @@
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     
-    if (textField.text.length >= PWD_COUNT && string.length) {
+    if (textField.text.length >= kPasswordCount && string.length) {
         //输入的字符个数大于6，则无法继续输入，返回NO表示禁止输入
         return NO;
     }
@@ -182,12 +194,12 @@
 }
 
 - (void)setDotWithCount:(NSInteger)count {
-    for (UILabel *dot in pwdIndicatorArr) {
+    for (UILabel *dot in self.pwdIndicators) {
         dot.hidden = YES;
     }
     
     for (int i = 0; i< count; i++) {
-        ((UILabel*)[pwdIndicatorArr objectAtIndex:i]).hidden = NO;
+        ((UILabel*)[self.pwdIndicators objectAtIndex:i]).hidden = NO;
     }
 }
 
@@ -205,6 +217,15 @@
 - (void)setAmount:(CGFloat)amount {
     _amount = amount;
     _amountLabel.text = [NSString stringWithFormat:@"￥%.2f  ",amount];
+}
+
+#pragma mark - Getter
+
+- (NSMutableArray *)pwdIndicators {
+    if (_pwdIndicators == nil) {
+        _pwdIndicators = [[NSMutableArray alloc]init];
+    }
+    return _pwdIndicators;
 }
 
 @end
